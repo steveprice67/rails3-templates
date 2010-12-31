aae1825dfce73a7f4864ec39d90b48ed4a585dac
Steve Price <steveprice67@gmail.com>
2010-12-30 22:40:09 -0600

    break-up a  really long line with a heredoc

--- basic.rb
+++ basic.rb
@@ -37,7 +37,10 @@ rake 'db:sessions:create'
 rake 'db:migrate'
 
 run 'rm -f config/initializers/session_store.rb'
-initializer 'session_store.rb', "#{app_const}.config.session_store :active_record_store, :key => '_#{app_name}_#{ActiveSupport::SecureRandom.hex(4)}_session'\n"
+initializer 'session_store.rb', <<-EOF
+#{app_const}.config.session_store :active_record_store,
+  :key => '_#{app_name}_#{ActiveSupport::SecureRandom.hex(4)}_session'
+EOF
 
 append_file 'Gemfile', <<-EOF
 group :test, :development do
