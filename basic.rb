30e2731578f6f3aff2dacd5c0073fef1896440da
Steve Price <steveprice67@gmail.com>
2010-12-30 22:27:24 -0600

    rm public/javascripts/rails.js so we don't get a conflict

--- basic.rb
+++ basic.rb
@@ -54,6 +54,7 @@ end
 EOF
 run 'bundle install'
 
+run 'rm -f public/javascripts/rails.js'
 initializer 'jquery-hack.rb', "OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE\n"
 generate 'jquery:install'
 run 'rm -f config/initializers/jquery-hack.rb'
